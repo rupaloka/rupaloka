f6cc82e8965ca65be8cc97f1765755d6e795da4e
rupaloka <wbn.heri@gmail.com>
2025-12-19 12:05:12 +0700

    Create invoice-list.js

---
+++ js/invoice-list.js
@@ -0,0 +1,32 @@
+import { db, collection, query, orderBy, onSnapshot } from "./firebase.js";
+
+const tbody = document.getElementById("invoiceTable");
+
+const rupiah = n =>
+  new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0
+  }).format(n);
+
+const q = query(collection(db, "invoices"), orderBy("createdAt", "desc"));
+
+onSnapshot(q, (snap) => {
+  tbody.innerHTML = "";
+  snap.forEach(doc => {
+    const i = doc.data();
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${i.projectName || "-"}</td>
+      <td>${i.clientName}</td>
+      <td>${i.description}</td>
+      <td>${rupiah(i.amount)}</td>
+      <td>
+        <button class="print" onclick="window.print()">Cetak</button>
+      </td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+});
